596c538bc2f1ccaa46b6bc2c97a5dcabc67c2a0d
Gerry Tan <2316198+gerrytan@users.noreply.github.com>
2026-09-10 14:39:24 +1000

    Revert to only resource-manager.hcl change

--- tools/generator-go-sdk/generated-sdk-dev/resource-manager/oracledatabase/2026-06-01/databaseeditions/model_databaseeditionproperties.go
+++
@@ -1,8 +0,0 @@
-package databaseeditions
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
-
-type DatabaseEditionProperties struct {
-	DatabaseEdition DbSystemDatabaseEditionType `json:"databaseEdition"`
-}
